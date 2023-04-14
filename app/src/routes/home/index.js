4251ea7101ea61dd40298aefc3d3bae1a24bdb10
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-04-14 09:22:03 +0900

    [FEAT]회원가입 유저폼 등록 페이지 html,css

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,5 +8,6 @@ const ctrl = require("./home.ctrl");
 
 router.get("/",ctrl.output.home);
 router.get("/signin",ctrl.output.signin);
+router.get("/signup",ctrl.output.signup);
 
 module.exports=router;
