76945b75a09b6de7fa6557e2e4d501a95360bdd4
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-05-03 01:26:08 +0900

    [FEAT]에러 처리를 위한 미들웨어,view 구현완료
    - 404 NOT FOUND view 구현 완료
    - 에러 처리를 위한 미들웨어인 errorController.js구현완료

--- app/main.js
+++ app/main.js
@@ -6,7 +6,8 @@ const bodyParser =require("body-parser");
 const app = express();
 
 // 라우팅
-const home = require("./src/controllers/home");
+const controllers = require("./src/controllers");
+const errorController=require("./src/controllers/errorController");
 //db
 const uniunity_db = require("./config/db");
 
@@ -19,7 +20,12 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 //URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({extended:true}));
-app.use("/",home); //use -> 미들 웨어를 등록해주는 메서드
+app.use("/",controllers); //use -> 미들 웨어를 등록해주는 메서드
+
+//에러처리를 위한 미들웨어 생성
+app.use(errorController.logErrors);
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalEroor);
 
 
 uniunity_db();
