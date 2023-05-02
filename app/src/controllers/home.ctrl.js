99d8ab2a11437413637bc0d13ba50a4917bc0878
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-05-03 01:23:42 +0900

    [REFACTOR]폴더명 변경 및 경로 수정

--- app/src/controllers/home.ctrl.js
+++ app/src/controllers/home.ctrl.js
@@ -1,16 +1,16 @@
 "use strict"
 
-const User =require("../../models/User");
+const User =require("../models/User");
 const output ={
     home : (req,res)=>{
-        res.render("home/index");
+        res.render("index");
     },
     
     login : (req,res)=>{
-        res.render("home/login");
+        res.render("login");
     },
     register:(req,res)=>{
-        res.render("home/register");
+        res.render("register");
     },
 }
 
