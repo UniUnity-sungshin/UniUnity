0d54eefa7469d1c29942568769aa4a07886222ad
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-05-08 02:56:09 +0900

    [FEAT]대학교 검색 엔진을 위한 UniversityNameList를 반환하는 API구현 완료
    - GET : /showUniversityNameList

--- app/src/controllers/index.js
+++ app/src/controllers/index.js
@@ -10,6 +10,6 @@ router.get("/",ctrl.output.home);
 router.get("/signin",ctrl.output.signin);
 router.get("/signup",ctrl.output.signup);
 
-router.get("/search/universityname/:keyword",ctrl.output.searchUniversityName);
+router.get("/showUniversityNameList",ctrl.output.showUniversityNameList);
 
 module.exports=router;
