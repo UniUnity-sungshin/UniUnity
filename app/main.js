4d2754c30a50a2dd3d7fc1c458a855107635b680
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-05-09 23:56:07 +0900

    [FEAT]User,UserStorage모델 생성 및 로그인 라우팅 추가

--- app/main.js
+++ app/main.js
@@ -3,13 +3,12 @@
 // 모듈
 const express = require("express");
 const bodyParser =require("body-parser");
+const passport = require("passport");
 const app = express();
-
+const pool=require('./config/db')
 // 라우팅
 const controllers = require("./src/controllers/index");
 const errorController=require("./src/controllers/errorController");
-//db
-const uniunity_db = require("./config/db");
 
 // 앱 셋팅
 // 서버가 읽을 수 있도록 HTML 의 위치를 정의해줍니다.
@@ -22,14 +21,81 @@ app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 //URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+
+app.use(require('serve-static')(__dirname + '/src/public'));
+app.use(require('cookie-parser')());
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use("/",controllers); //use -> 미들 웨어를 등록해주는 메서드
 
 //에러처리를 위한 미들웨어 생성
-// app.use(errorController.logErrors);
-// app.use(errorController.respondNoResourceFound);
-// app.use(errorController.respondInternalEroor);
+app.use(errorController.logErrors);
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalEroor);
 
-app.use(controllers);
+
+// passport.serializeUser(function(user, done) {
+//     console.log("serializeUser ", user)
+//     done(null, user.ID);
+//   });
+  
+//   passport.deserializeUser(function(id, done) {
+//       console.log("deserializeUser id ", id)
+//       var userinfo;
+//       var sql = 'SELECT * FROM User WHERE uesr_email=?';
+//       pool.getConnection((err,connection)=>{
+//         if(err){
+//             console.error('MySQL 연결 오류: ',err);
+//             throw err;
+//         }
+    
+//         });
+    
+//       pool.query(sql , [id], function (err, result) {
+//         if(err) console.log('mysql 에러');     
+       
+//         console.log("deserializeUser mysql result : " , result);
+//         var json = JSON.stringify(result[0]);
+//         userinfo = JSON.parse(json);
+//         done(null, userinfo);
+//       })    
+//   });
+
+//   router.get('/login', function(req, res, next) {
+//     var userId = "";
+//     if(req.cookies['loginId'] !== undefined){
+//       console.log(req.cookies['loginId']);
+//       userId = req.cookies['rememberId'];
+//     }
+//     res.render('login', {userId: userId});
+//   });
+  
+//   passport.use(new LocalStrategy({
+//       usernameField: 'user_email',
+//       passwordField: 'psword'
+//     },
+//     function(username, password, done) {
+//       var sql = 'SELECT * FROM User WHERE user_email=? AND psword=?';
+//       pool.query(sql , [user_email, psword], function (err, result) {
+//         if(err) console.log('mysql 에러');  
+  
+//         // 입력받은 ID와 비밀번호에 일치하는 회원정보가 없는 경우   
+//         if(result.length === 0){
+//           console.log("결과 없음");
+//           return done(null, false, { message: 'Incorrect' });
+//         }else{
+//           console.log(result);
+//           var json = JSON.stringify(result[0]);
+//           var userinfo = JSON.parse(json);
+//           console.log("userinfo " + userinfo);
+//           return done(null, userinfo);  // result값으로 받아진 회원정보를 return해줌
+//         }
+//       })
+//     }
+//   ));
+
 
 module.exports =app;
--- app/src/controllers/index.js
+++ app/src/controllers/index.js
@@ -7,9 +7,16 @@ const ctrl = require("./home.ctrl");
 
 
 router.get("/",ctrl.output.home);
-router.get("/signin",ctrl.output.signin);
+router.get("/login",ctrl.output.login);
 router.get("/signup",ctrl.output.signup);
-
 router.get("/showUniversityNameList",ctrl.output.showUniversityNameList);
+
+
+router.post("/login",ctrl.process.login);
+// router.post('/login',
+//   passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
+//   function(req, res) {
+//     res.redirect('/' + req.user.universirty_id);
+//   });
 
 module.exports=router;
