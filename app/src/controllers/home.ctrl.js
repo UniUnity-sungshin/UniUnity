6240970dffccd10c70ba6062b37fb3de51ea5209
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-05-10 23:57:30 +0900

    [FEAT]사용자 로그인 기능 추가 및 라우팅 완료(세션x)
    - 사용자 로그인 기능 추가 및 라우팅 완료
    - 세션과 비밀번호 해싱 추가해야함

--- app/src/controllers/home.ctrl.js
+++ app/src/controllers/home.ctrl.js
@@ -2,8 +2,6 @@
 
 const University = require("../models/University");
 const User =require("../models/User");
-// const Partner = require("../models/Partner");
-
 const output ={
     home : (req,res)=>{
         res.render('home/mainpage.html');
@@ -14,37 +12,29 @@ const output ={
     signup : (req,res)=>{
         res.render('home/signup.html');
     },
-    showUniversityNameList:async(req,res)=>{
-        const university_name=new University();
-        const response=await university_name.showUniversityNameList();
-        return res.json(response);
-
-    },
     partner:(req,res)=>{
-        res.render("home/partner.html");
-    },
-    // partnerUni:(req,res)=>{
-    //     res.render('/partner/:university.html');
-    // },
+            res.render("home/partner.html");
+    }
 }
 
-//로그인 인증 process
+
 const process={
+    //로그인 인증 process
     login:async (req,res)=>{
-        console.log(req);
-        console.log(req.body);
+        console.log("1",req.body);
         const user= new User(req.body);
         const response=await user.login();
-        const url ={
-            method:"POST",
-            path:"/login",
-            status: response.arr?400:200,
-        };
-        return res.status(url.status).json(response);
+        console.log("login response:",response);
+        return res.json(response);
     },
     register:(req,res)=>{
         const user= new User(req.body);
         const response=user.register();
+        return res.json(response);
+    },
+    showUniversityNameList:async(req,res)=>{
+        const university_name=new University();
+        const response=await university_name.showUniversityNameList();
         return res.json(response);
     },
     
