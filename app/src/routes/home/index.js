e100a69cd9f8eb23c952366bb9719d5d5ae5591b
youngseoOh <dudtj2379@gmail.com>
2023-05-11 14:13:16 +0900

    swiper implementation

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -13,7 +13,14 @@ router.get("/retailer",ctrl.output.retailer);
 router.get("/partner",ctrl.output.partner);
 router.get("/partner/:university",ctrl.output.partnerUni);
 
-router.post("/login",ctrl.process.login);
-router.post("/register",ctrl.process.register);
+//council 페이지 라우팅
+router.get("/sungshin", ctrl.result.council);
+router.get("/sungshin/storeMap", ctrl.result.storemap);
+router.get("/sungshin/affiliationMap", ctrl.result.affiliationmap);
+router.get("/sungshin/myPage", ctrl.result.mypage);
+router.get("/sungshin/moreNews", ctrl.result.morenews);
+router.get("/sungshin/moreStore", ctrl.result.morestore);
+router.get("/sungshin/moreSchool", ctrl.result.moreschool);
+router.get("/sungshin/mainNews", ctrl.result.mainnews);
 
 module.exports=router;
