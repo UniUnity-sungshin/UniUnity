4097b5f0e8fa066a1fdcccceed7e51727c761349
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-05-12 01:21:39 +0900

    [FIX]db.js/index.js/home.ctrl.js 지금까지 최종, 여기에 라우터 추가해주세요. 절대 db.js변경 금지
    - index.js, home.ctl.js merge하면서 막 바뀌는데 이 최종본에 여러분의 코드를 추가해줘요...이 코드를 변경하지 말아줘요.
    - db.js 절대 바꾸지 말아줘요...

--- app/config/db.js
+++ app/config/db.js
@@ -2,22 +2,6 @@
 require('dotenv').config();
 
 const mysql = require('mysql2/promise');
-// let  = async()=>{
-//     const db=mysql.createPool({
-//         host:process.env.DB_HOST,
-//         user:process.env.DB_USER,
-//         password:process.env.DB_PW,
-//         port:process.env.DB_PORT,
-//         database:process.env.DB_NAME,
-//         waitForConnections:true,
-//         insecureAuth:true
-//     });
-//     const sql=`SELECT university_name from University`;
-            
-//     let [rows,fields]=await db.query(sql);
-//     console.log(rows);
-
-// };
 
 // MySQL 연결 풀 생성
 
@@ -31,14 +15,4 @@ const mysql = require('mysql2/promise');
         insecureAuth:true
 });
 
-const db = {
-  getConnection : (callback) => {
-    pool.getConnection((err, conn) => {
-      if (err) throw err;
-      callback(conn);
-    });
-  }
-}
-
-
-module.exports=db;
+module.exports=pool;
--- app/src/controllers/index.js
+++ app/src/controllers/index.js
@@ -6,13 +6,14 @@ const router = express.Router();
 const ctrl = require("./home.ctrl");
 
 router.get("/",ctrl.output.home);
-router.get("/signin",ctrl.output.signin);
+router.get("/login",ctrl.output.login);
 router.get("/signup",ctrl.output.signup);
+router.get("/showUniversityNameList",ctrl.output.showUniversityNameList);
 router.get("/:universityname",ctrl.output.council);
 router.get("/council/:universityname",ctrl.result.council);
 
-router.get("/search/universityname/:keyword",ctrl.output.searchUniversityName);
 
+router.post("/login",ctrl.process.login);
 //council 페이지 라우팅
 // router.get("/sungshin\", ctrl.result.council);
 
