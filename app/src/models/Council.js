6691bd63acffe01eca0ef2a8fc4dcdf204b4b97e
youngseoOh <dudtj2379@gmail.com>
2023-05-12 17:46:22 +0900

    <% 태그, 이미지 안 뜨는 문제

--- app/src/models/Council.js
+++ app/src/models/Council.js
@@ -7,10 +7,8 @@ class Council{
     async showUniversity(universityName){
         try{
             const response=await CouncilStorage.getUniversity(universityName);
-            console.log("Council.js에서 showUniversity 성공");
             return response;
         }catch(err){
-            console.log("showUniversity 에러");
             return{success:false,msg:err};
         }
     }
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -14,13 +14,7 @@ router.get("/partner",ctrl.output.partner);
 router.get("/partner/:university",ctrl.output.partnerUni);
 
 //council 페이지 라우팅
-router.get("/sungshin", ctrl.result.council);
-router.get("/sungshin/storeMap", ctrl.result.storemap);
-router.get("/sungshin/affiliationMap", ctrl.result.affiliationmap);
-router.get("/sungshin/myPage", ctrl.result.mypage);
-router.get("/sungshin/moreNews", ctrl.result.morenews);
-router.get("/sungshin/moreStore", ctrl.result.morestore);
-router.get("/sungshin/moreSchool", ctrl.result.moreschool);
-router.get("/sungshin/mainNews", ctrl.result.mainnews);
+//router.get("/sungshin", ctrl.result.council);
+//router.get("/council/sungshin", ctrl.result.council);
 
 module.exports=router;
