92ec5f7f3428195f670ef5ad339201ccd0ba6a95
youngseoOh <dudtj2379@gmail.com>
2023-05-16 09:41:01 +0900

    학교 이름 불러오기 함수 수정

--- app/src/controllers/home.ctrl.js
+++ app/src/controllers/home.ctrl.js
@@ -59,14 +59,13 @@ const partner = {
 
 //council 페이지 라우팅
 const result = {
-    council :async (req, res) => {
+    council : async (req, res) => {
         console.log(req.params.universityname);
         const council = new Council();
         const response=await council.showUniversity(req.params.universityname);
         console.log(response);
-        res.render("council/council.ejs", {data: response});
-        
-        //return res.json(response);
+        res.render("council/council.html", {data: response});
+        //return response;
     }
 }
    
