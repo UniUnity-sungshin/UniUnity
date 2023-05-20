8167d39f064acabf6b0a4be5ba3e4c3131f499bd
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-05-20 22:21:42 +0900

    [FEAT]university_id받아 university_name반환 함수 추가

--- app/src/models/University.js
+++ app/src/models/University.js
@@ -4,6 +4,15 @@ class University{
     constructor(body){
         this.body=body;
     }
+    async getUnversityIdToName(university_id){
+        try{
+            const response=await UniversityStorage.getUnversityName(university_id);
+            console.log("getUnversityIdToName",university_id,response);
+            return response;
+        }catch(err){
+            return{success:false,msg:err};
+        }
+    }
     async showUniversityNameList(){
         try{
             const response=await UniversityStorage.getUniversityNameList();
