d04033d3683ebd5c55418ed9dc8ecc87cff34618
Jihyeon1520 <82721608+hyeon1520@users.noreply.github.com>
2023-05-20 19:40:13 +0900

    [FEAT] 소상공인 페이지 구현 1차본

--- app/src/public/js/home/ratailerPage.js
+++ app/src/public/js/home/ratailerPage.js
@@ -1,97 +1,95 @@
 "use strict";
 
+// const { error } = require('console');
+// 공공데이터 소상공인 상가 정보 OPEN API 사용
+// const { resolve } = require('path');
+// const request = require('request');
+// const { reject } = require('underscore');
+const serviceKey = 'p0%2BHQGnCYhn4J%2BB0BJpY5cOD0thCQ29az7PS9MQ4gLwPqbZrSns3eFy4VZ%2BUSc95PAkZUjK%2FGiir%2FcMk1FAq4A%3D%3D';
+const endPoint = 'http://apis.data.go.kr/B553077/api/open/sdsc2/';
+
+// 고정 지도 코드
+// ===========================================================================================
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-               mapOption = { 
-                   center: new kakao.maps.LatLng(37.5912999, 127.0221068), // 지도의 중심좌표
-                   level: 3 // 지도의 확대 레벨
-               };
+    mapOption = { 
+        center: new kakao.maps.LatLng(37.5667, 126.9783),
+        level: 3 // 지도의 확대 레벨
+    };
 
-           var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-           //********* 지도 이동 이벤트 *********//
-           function setCenter() {            
-               // 이동할 위도 경도 위치를 생성합니다 
-               var moveLatLon = new kakao.maps.LatLng(33.452613, 126.570888);
-               
-               // 지도 중심을 이동 시킵니다
-               map.setCenter(moveLatLon);
-           }
+var stores = [];
+var positions = [];
 
-            function panTo() {
-               // 이동할 위도 경도 위치를 생성합니다 
-               var moveLatLon = new kakao.maps.LatLng(33.450580, 126.574942);
-               
-               // 지도 중심을 부드럽게 이동시킵니다
-               // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
-               map.panTo(moveLatLon);            
-           }        
+const storeName = document.querySelector('#storeName'),
+      storeAdr = document.querySelector('#storeAdr'),
+      storeClass = document.querySelector('#storeClass');
+const storeInfoTextBox = document.querySelectorAll(".storeInfoTextBox");
 
-           //********* 지도 클릭 시 마커 표시 이벤트 *********//
-           // 지도에 클릭 이벤트를 등록합니다
-           // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
-           kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
-               
-               // 클릭한 위도, 경도 정보를 가져옵니다 
-               var latlng = mouseEvent.latLng; 
-               
-               // 마커 위치를 클릭한 위치로 옮깁니다
-               marker.setPosition(latlng);
-           });
+// 지도가 이동, 확대, 축소로 인해 지도영역이 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+kakao.maps.event.addListener(map, 'bounds_changed', function() {             
+    
+    // 지도 영역정보를 얻어옵니다 
+    var bounds = map.getBounds();
+    
+    // 영역정보의 남서쪽 정보를 얻어옵니다 
+    // swLatlng.La = 서쪽 경도좌표값 = minx
+    // swLatlng.Ma = 남쪽 경도좌표값 = miny
+    var swLatlng = bounds.getSouthWest();
+    var minx = swLatlng.La.toString(),
+        miny = swLatlng.Ma.toString();
 
-           //********* 지도 마커 클릭 이벤트 *********//
-           // 마커를 표시할 위치입니다 
-           var position =  new kakao.maps.LatLng(33.450701, 126.570667);
+    // 영역정보의 북동쪽 정보를 얻어옵니다 
+    // neLatlng.La = 동쪽 경도좌표값 = maxx
+    // neLatlng.Ma = 북쪽 경도좌표값 = maxy
+    var neLatlng = bounds.getNorthEast();
+    var maxx = neLatlng.La.toString(),
+        maxy = neLatlng.Ma.toString();
 
-           // 마커를 생성합니다
-           var marker = new kakao.maps.Marker({
-           position: position,
-           clickable: true // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-           });
-
-           // 아래 코드는 위의 마커를 생성하는 코드에서 clickable: true 와 같이
-           // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-           // marker.setClickable(true);
-
-           // 마커를 지도에 표시합니다.
-           marker.setMap(map);
-
-           // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-           var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-               iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-
-           // 인포윈도우를 생성합니다
-           var infowindow = new kakao.maps.InfoWindow({
-               content : iwContent,
-               removable : iwRemoveable
-           });
-
-           // 마커에 클릭이벤트를 등록합니다
-           kakao.maps.event.addListener(marker, 'click', function() {
-               // 마커 위에 인포윈도우를 표시합니다
-               infowindow.open(map, marker);  
-           });
-
-function retailer(){
-
-    fetch("/retailer",{
-        method:"GET",
-        headers:{
-            "Content-Type" :"application/json",
-        },
-        body:JSON.stringify(req),
-    })
-    .then((res)=>res.json())
-    .then((res)=>{
-        if(res.success){
-            location.href ="/";
-        }else{
-            alert(res.msg);
+    var url = endPoint + 'storeListInRectangle' + '?serviceKey=' + serviceKey + '&pageNo=1' + '&numOfRows=10' + 
+            '&minx=' + minx + '&miny=' + miny + '&maxx=' + maxx + '&maxy=' + maxy + '&type=json';
+    fetch(url)
+    .then((res) => res.json())
+    .then(res => {
+        for(let i = 0; i < res.body.items.length; i++){
+            const obj = {
+                storeName: res.body.items[i].bizesNm,
+                store_location: res.body.items[i].rdnmAdr,
+                storeClass: res.body.items[i].indsLclsNm,
+                storeItem: res.body.items[i].indsSclsNm
+            };
+            stores.push(obj);
+            positions.push(new kakao.maps.LatLng(res.body.items[i].lat,res.body.items[i].lon));
         }
+        // console.log(res.body.items.length);
     })
-    .catch((err)=>{
-        console.log(err);
+    .catch(error => {
+        console.log('Error:', error);
     });
-}
+    for (let i = 0; i < positions.length; i ++) {
+        // 마커를 생성합니다
+        let marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: positions[i] // 마커의 위치
+        });
+        // 마커 click, mouseover, mouseout 시에 이벤트 발생
+        kakao.maps.event.addListener(marker, 'click', function(){
+            for(let i = 0; i < storeInfoTextBox.length; i++){
+                storeInfoTextBox[i].style.display = "block";
+            }
+            storeName.innerHTML = stores[i].storeName;
+            storeAdr.innerHTML = stores[i].store_location;
+            storeClass.innerHTML = stores[i].storeClass;
+        });
+    }
+});
+
+// ===========================================================================================
+
+
+
+
+// window.addEventListener('DOMContentLoaded', retailerLoad);
 
 // app.get('/retailer', function(req, res){
 //     request(test, function(error, response, body){
@@ -103,3 +101,35 @@ function retailer(){
 //       console.log(obj.body.items[0].trarNo);
 //     })
 //   })
+
+// function retailerLoad(){
+//     // 행정구역단위 상권조회
+//     let apiAdr = endPoint + 'storeZoneInAdmi?' + serviceKey + 'divId=' + 'signguCd'+ '&key=' + '11290' + '&type=json';
+//     fetch(apiAdr)
+//     .then((res)=>res.json())
+//     .then((res)=>{
+//         let obj = JSON.parse(res);
+//         console.log(obj.body.items[0]);
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     });
+// }
+
+// const loadData = useCallback(async () => {
+//     try {
+//       await axios
+//         .get(
+//           `http://apis.data.go.kr/B553077/api/open/sdsc2/storeListInRectangle?serviceKey=${serviceKey}&pageNo=1&numOfRows=10&minx=${minx}'&miny=${miny}&maxx=${maxx}`,
+//         )
+//         .then((res) => {
+//           const jsonRes = res.data.data;
+//         });
+//     } catch (error) {
+//       console.log(error);
+//     }
+// });
+
+// useEffect(() => {
+//     loadData();
+//   }, [loadData]);
