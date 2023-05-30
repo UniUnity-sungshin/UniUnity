795a953252814266257fab51a6971662e7558583
Jihyeon1520 <82721608+hyeon1520@users.noreply.github.com>
2023-05-30 16:43:41 +0900

    [FEAT]제휴 상가 등록

--- app/src/public/js/post/postform.js
+++ app/src/public/js/post/postform.js
@@ -1,5 +1,7 @@
 const Editor = toastui.Editor;
 
+
+
 const editor = new Editor({
     el: document.querySelector('#editor'),
     height: '500px',
@@ -8,3 +10,18 @@ const editor = new Editor({
   });
   
   editor.getMarkdown();
+
+
+// 제휴가게 등록하기 폼
+// ===========================================================================================
+// 기본 좌표 저징 지도 코드
+// ===========================================================================================
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };  
+
+// 지도를 생성합니다    
+var map = new kakao.maps.Map(mapContainer, mapOption); 
+// ===========================================================================================
