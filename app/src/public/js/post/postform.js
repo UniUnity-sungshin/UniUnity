59d24ec0c0f9e39a6669cd544faa6ef94c179a17
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-05-30 19:01:23 +0900

    [FEAT]글작성 페이지 카테고리 선택 추가

--- app/src/public/js/post/postform.js
+++ app/src/public/js/post/postform.js
@@ -1,3 +1,4 @@
+//글 에디터
 const Editor = toastui.Editor;
 
 const editor = new Editor({
@@ -8,3 +9,13 @@ const editor = new Editor({
   });
   
   editor.getMarkdown();
+
+  
+//카테고리 선택
+var selectedValue = "";
+  var selectPostCategoryElement = document.getElementById('select_post_category');
+  selectPostCategoryElement.addEventListener('change', function () {
+    selectedValue = this.value;
+    console.log(selectedValue); //제휴 소식은 선택시 콘솔에 1로 나옴
+
+});
