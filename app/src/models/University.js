00224a0da555cef26f40a7a9f02c907a21013348
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-06-03 18:33:58 +0900

    [FEAT]University,UniversityStorage모델 함수 추가
    - university_id받아 university_url반환하는 함수 추가

--- app/src/models/University.js
+++ app/src/models/University.js
@@ -13,6 +13,14 @@ class University{
             return{success:false,msg:err};
         }
     }
+    async getUnversityIdToUrl(university_id){
+        try{
+            const response=await UniversityStorage.getUnversityUrl(university_id);
+            return response;
+        }catch(err){
+            return{success:false,msg:err};
+        }
+    }
     async showUniversityNameList(){
         try{
             const response=await UniversityStorage.getUniversityNameList();
