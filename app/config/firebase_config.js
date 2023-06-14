28fa6ceb7e4432eb898ba86171eb2c5a9b2af38e
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-06-14 19:04:11 +0900

    [CHORE]firebase 정보

---
+++ app/config/firebase_config.js
@@ -0,0 +1,11 @@
+
+module.exports={
+    firebaseConfig:{
+        apiKey: "AIzaSyD5E7ZajvnV2-iJbSa9CMt78ssUfPUIbEU",
+        authDomain: "uniunity-383602.firebaseapp.com",
+        projectId: "uniunity-383602",
+        storageBucket: "uniunity-383602.appspot.com",
+        messagingSenderId: "664017190069",
+        appId: "1:664017190069:web:dbb0c0990980179f85d2fc"
+    }
+}
