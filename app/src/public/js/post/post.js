9c7556f79a661975f6644346cb1702d2b961fa6e
imeunyoung <80439953+imeunyoung@users.noreply.github.com>
2023-08-03 22:07:32 +0900

    게시글삭제 라우터변경
    delete->post
    삭제기능 서버측만 있는상태

--- app/src/public/js/post/post.js
+++ app/src/public/js/post/post.js
@@ -12,28 +12,28 @@ const loadloginData = () => {
     })
 };
 //로그인(로그아웃), 회원가입(마이페이지)버튼
-const loginStatusBtn=document.getElementById("loginStatusBtn");
-const signUpBtn=document.getElementById("signUpBtn");
-const navBar=document.getElementById("navbar-brand");
+const loginStatusBtn = document.getElementById("loginStatusBtn");
+const signUpBtn = document.getElementById("signUpBtn");
+const navBar = document.getElementById("navbar-brand");
 
-const setLoginHeader=(res)=>{
+const setLoginHeader = (res) => {
   navBar.setAttribute("href", `${apiUrl}/showPostListAll/${university_url}`);
-  if(res.loginStatus){
-      loginStatusBtn.setAttribute("href", `${apiUrl}/logout`);
-      loginStatusBtn.innerText="로그아웃"
-      signUpBtn.setAttribute("href", `${apiUrl}/mypage`);
-      signUpBtn.innerText="마이페이지"
+  if (res.loginStatus) {
+    loginStatusBtn.setAttribute("href", `${apiUrl}/logout`);
+    loginStatusBtn.innerText = "로그아웃"
+    signUpBtn.setAttribute("href", `${apiUrl}/mypage`);
+    signUpBtn.innerText = "마이페이지"
   }
-  else{
-      loginStatusBtn.setAttribute("href", `${apiUrl}/login`);
-      loginStatusBtn.innerText="로그인"
-      signUpBtn.setAttribute("href", `${apiUrl}/signup`);
-      signUpBtn.innerText="회원가입"
+  else {
+    loginStatusBtn.setAttribute("href", `${apiUrl}/login`);
+    loginStatusBtn.innerText = "로그인"
+    signUpBtn.setAttribute("href", `${apiUrl}/signup`);
+    signUpBtn.innerText = "회원가입"
   }
-  
+
 }
 const writePostBtn = document.getElementById('write_post_btn');
-const brandNav= document.getElementById('navbar-brand');
+const brandNav = document.getElementById('navbar-brand');
 
 writePostBtn.addEventListener('click', function () {
   if (userInfo.loginStatus) {
@@ -53,15 +53,15 @@ var university_url = currentUrl.split("/").pop();
 changeUniversityName(university_url) //학교 이름으로 변경
 
 //학교 이름 바꾸기
-function changeUniversityName(university_url){
+function changeUniversityName(university_url) {
   const universityNameElement = document.getElementById('university_name');
-  if(university_url==="sungshin"){
-    universityNameElement.textContent = '성신여자대학교 Unity'; 
-  }else if(university_url==="konkuk"){
-    universityNameElement.textContent = '건국대학교 Unity'; 
-  }else{
-    universityNameElement.textContent = 'Unity'; 
-  }  
+  if (university_url === "sungshin") {
+    universityNameElement.textContent = '성신여자대학교 Unity';
+  } else if (university_url === "konkuk") {
+    universityNameElement.textContent = '건국대학교 Unity';
+  } else {
+    universityNameElement.textContent = 'Unity';
+  }
 }
 
 // 카테고리 버튼 요소들을 선택합니다.
@@ -75,9 +75,9 @@ const storePromotionBtn = document.getElementById('store_promotion');
 const row = document.querySelector('.row');
 const col = document.querySelector('.col');
 
-affiliateRegistrationBtn.addEventListener('click',function(){
-    window.location.href = `${apiUrl}/partner/${university_url}`; // 제휴 등록은 제휴가게 페이지로 이동
-        return; // 리다이렉션 후 함수 종료
+affiliateRegistrationBtn.addEventListener('click', function () {
+  window.location.href = `${apiUrl}/partner/${university_url}`; // 제휴 등록은 제휴가게 페이지로 이동
+  return; // 리다이렉션 후 함수 종료
 })
 
 let currentCategory = ""; // 선택한 카테고리를 기억하는 변수
@@ -128,10 +128,10 @@ const fetchpostAllData = async () => {
   const data = await response.json();
   dataLength = data.length;
   const postsToShow = data.slice(startIndex, endIndex);
-  postsToShowLength = postsToShow.length; 
-  
+  postsToShowLength = postsToShow.length;
+
   const cardContainer = document.getElementById("card_container");
-  
+
   if (!cardContainer) {
     //console.error("card_container is null.");
     return;
@@ -240,7 +240,7 @@ function createCard(data) {
 }
 
 //로고 클릭시 postAllData()실행
-brandNav.addEventListener('click',function(){
+brandNav.addEventListener('click', function () {
   fetchpostAllData();
 });
 
@@ -255,55 +255,55 @@ function getUniversityUrl() {
 const postSearchBtn = document.querySelector('#postSearchBtn');
 var university_posts = [];
 const blogEntriesDiv = document.querySelector(".blog-entries");
-const cardContainer=document.getElementById("card_container");
+const cardContainer = document.getElementById("card_container");
 
-function searchPost(){
+function searchPost() {
   const universityUrl = getUniversityUrl();
   fetch(`${apiUrl}/getUniversityID/${universityUrl}`)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(response => {
-    // university_id를 university_posts[0]에 저장
-    university_posts.push(response);
-    // 검색한 게시글 불러오기
-    const keyword = document.getElementById('postSearchInput').value;
-    fetch(`${apiUrl}/searchPost/${keyword}`)
-    .then((res) => {
-      if (!res.ok) {
+    .then((response) => {
+      if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      return res.json();
+      return response.json();
     })
-    .then(res => {
-      for(let i = 0; i < res.length; i++){
-        if(String(res[i].university_id) == String(university_posts[0])){
-          university_posts.push(res[i]);
-        }
-      }
-      // 기존 게시글 지운 후 검색된 게시글 나열
-      while (cardContainer.firstChild) {
-        cardContainer.removeChild(cardContainer.firstChild);
-      }
-      for (var i = 0; i < res.length; i++) {
-        createCard(res[i])
-      }
+    .then(response => {
+      // university_id를 university_posts[0]에 저장
+      university_posts.push(response);
+      // 검색한 게시글 불러오기
+      const keyword = document.getElementById('postSearchInput').value;
+      fetch(`${apiUrl}/searchPost/${keyword}`)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return res.json();
+        })
+        .then(res => {
+          for (let i = 0; i < res.length; i++) {
+            if (String(res[i].university_id) == String(university_posts[0])) {
+              university_posts.push(res[i]);
+            }
+          }
+          // 기존 게시글 지운 후 검색된 게시글 나열
+          while (cardContainer.firstChild) {
+            cardContainer.removeChild(cardContainer.firstChild);
+          }
+          for (var i = 0; i < res.length; i++) {
+            createCard(res[i])
+          }
 
+        })
+        .catch((error) => {
+          console.error('There has been a problem with your fetch operation:', error);
+        });
     })
     .catch((error) => {
       console.error('There has been a problem with your fetch operation:', error);
     });
-    })
-  .catch((error) => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
 }
 
-postSearchBtn.addEventListener('click',searchPost);
-document.addEventListener('keydown', function(event) {
+postSearchBtn.addEventListener('click', searchPost);
+document.addEventListener('keydown', function (event) {
   if (event.keyCode === 13) {
     event.preventDefault();
   };
@@ -325,11 +325,11 @@ const lastpage = document.getElementById('last_page'); // 마지막 15 페이지
 
 // Add event listeners to each page button
 for (let i = 0; i < pageBtns.length; i++) {
-  pageBtns[i].addEventListener('click', function() {
-      const clickedPageNum = parseInt(this.firstChild.innerText);
-      if (!isNaN(clickedPageNum)) {
-          goToPage(clickedPageNum);
-      }     
+  pageBtns[i].addEventListener('click', function () {
+    const clickedPageNum = parseInt(this.firstChild.innerText);
+    if (!isNaN(clickedPageNum)) {
+      goToPage(clickedPageNum);
+    }
   });
 }
 
@@ -396,7 +396,7 @@ function initializeBtns() {
 function goToPage(pageNum) {
   currentPage = pageNum;
   console.log("Navigating to page: " + currentPage);
-  fetchPosts(currentCategory, university_url); 
+  fetchPosts(currentCategory, university_url);
 }
 
 // 다음 페이지로 이동 함수
@@ -422,15 +422,59 @@ function goToPrevPage() {
 }
 
 // 다음/이전 페이지 이벤트리스너
-newer.addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function () {
+newer.addEventListener('click', function () {
   if (currentPage >= 2) {
     goToPrevPage();
   }
-});
+})
 
-older.addEventListener('click', function() {
+older.addEventListener('click', function () {
   if (currentPage <= 14) {
     goToNextPage();
   }
-});
+})
+})
+// document.addEventListener('DOMContentLoaded', function() {
+//   var newer = document.getElementById('newer');
+//   var older = document.getElementById('older');
+//   var currentPage = 2; // Assume you have defined currentPage somewhere.
+
+//   if (newer) {
+//     newer.addEventListener('click', function() {
+//       if (currentPage >= 2) {
+//         goToPrevPage();
+//       }
+//     });
+//   } else {
+//     console.error("Element with id 'newer' not found.");
+//   }
+
+//   if (older) {
+//     older.addEventListener('click', function() {
+//       if (currentPage <= 14) {
+//         goToNextPage();
+//       }
+//     });
+//   } else {
+//     console.error("Element with id 'older' not found.");
+//   }
+// });
+
+// your_controller_file_path.js
+const doDeletePost = (req, res) => {
+  const post_id = req.params.post_id;
+  const user_email = req.body.user_email; // 요청 본문에서 user_email을 가져옵니다.
+
+  // post_id와 user_email을 사용하여 게시글을 삭제하는 로직을 여기에 작성합니다.
+  // 예를 들어, 게시글 삭제를 처리하는 deletePost 함수를 사용한다고 가정합니다.
+  doDeletePost(post_id, user_email)
+    .then(() => {
+      res.status(200).json({ message: '게시글이 성공적으로 삭제되었습니다!' });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: '게시글 삭제 중에 오류가 발생했습니다.' });
+    });
+};
+
 
