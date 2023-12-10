73ec7ad7a92608bb8f3c2f5bbb1e7938a20e9e50
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-12-10 06:30:08 +0000

    [TEST] www.js파일 TEST코드 작성

--- app/bin/www.js
+++ app/bin/www.js
@@ -3,6 +3,11 @@
 const app = require("../main");
 const PORT = 80;
 
-app.listen(PORT, ()=>{
+app.get('/test', (req,res)=>{
+    console.log('request test');
+    res.send("TEST");
+});
+
+app.listen(PORT, () =>{
     console.log("서버 가동")
 });
