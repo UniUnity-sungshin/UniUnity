4977a2279a182127bd9eeb4df9998100dc336789
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-12-14 02:29:01 +0900

    [REFACTOR] 쿠버네티스 시크릿을 사용하도록 db.js코드 수정

--- app/config/db.js
+++ app/config/db.js
@@ -1,23 +1,39 @@
-require('dotenv').config();
+const k8s = require('@kubernetes/client-node');
 
-const mysql = require('mysql2/promise');
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
-const pool = mysql.createPool({
-    host:process.env.DB_HOST,
-        user:process.env.DB_USER,
-        port:process.env.DB_PORT,
-        database:process.env.DB_NAME,
-        password:process.env.DB_PW,
-        waitForConnections:true,
-        insecureAuth:true,
-        dateStrings : "date",
-        connectionLimit: 1000,
-        waitForConnections: false
+const secretName = 'db-secret';
+const namespace = 'uniunity';  // 네임스페이스에 주의
 
+// 시크릿 가져오기
+k8sApi.readNamespacedSecret(secretName, namespace).then((response) => {
+  // 환경 변수 설정
+  const dbHost = Buffer.from(response.body.data.DB_HOST, 'base64').toString('utf-8');
+  const dbPort = Buffer.from(response.body.data.DB_PORT, 'base64').toString('utf-8');
+  const dbUser = Buffer.from(response.body.data.DB_USER, 'base64').toString('utf-8');
+  const dbPassword = Buffer.from(response.body.data.DB_PW, 'base64').toString('utf-8');
+  const dbName = Buffer.from(response.body.data.DB_NAME, 'base64').toString('utf-8');
+
+  // MySQL 연결 풀 생성
+  const mysql = require('mysql2/promise');
+
+  const pool = mysql.createPool({
+    host: dbHost,
+    user: dbUser,
+    port: dbPort,
+    database: dbName,
+    password: dbPassword,
+    waitForConnections: true,
+    insecureAuth: true,
+    dateStrings: "date",
+    connectionLimit: 1000,
+    waitForConnections: false
   });
-console.log(DB_HOST);
 
-module.exports=pool;
-
-
-
+  console.log(dbHost,dbName,dbUser);  // 이제는 변수를 직접 사용
+  module.exports = pool;
+}).catch((err) => {
+  console.error('Error reading secret:', err);
+});
