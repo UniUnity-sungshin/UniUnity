746208685b9a2dfa2d838a97fc0c7a3ae7d9c5a8
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-12-13 16:13:25 +0900

    [REFACTOR]db.js 쿠버네티스 secret 파일 정보 불러올 수 있도록 변경

--- app/config/db.js
+++ app/config/db.js
@@ -1,15 +1,37 @@
-//db.js
-require('dotenv').config();
+
+const k8s = require('@kubernetes/client-node');
+
+// Kubernetes 클라이언트 구성
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+// 쿠버네티스 API 사용
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+// 시크릿 이름과 네임스페이스 설정
+const secretName = 'db-secrets';
+const namespace = 'uniunity';
 
 const mysql = require('mysql2/promise');
 
-// MySQL 연결 풀 생성
+// 시크릿 가져오기
+k8sApi.readNamespacedSecret(secretName, namespace).then((response) => {
+  const dbHost = Buffer.from(response.body.data.DB_HOST, 'base64').toString('utf-8');
+  const dbPort = Buffer.from(response.body.data.DB_PORT, 'base64').toString('utf-8');
+  const dbUser = Buffer.from(response.body.data.DB_USER, 'base64').toString('utf-8');
+  const dbPassword = Buffer.from(response.body.data.DB_PASSWORD, 'base64').toString('utf-8');
+  const dbName = Buffer.from(response.body.data.DB_NAME, 'base64').toString('utf-8');
+
+  // 이제 dbHost, dbPort, dbUser, dbPassword, dbName 변수를 사용하여 DB에 연결할 수 있습니다.
+
+  // MySQL 연결 풀 생성
 
   const pool = mysql.createPool({
-    host:process.env.DB_HOST,
-        user:process.env.DB_USER,
-        port:process.env.DB_PORT,
-        database:process.env.DB_NAME,
+    host:dbHost,
+        user:dbUser,
+        port:dbPort,
+        database:dbName,
+        password:dbPassword,
         waitForConnections:true,
         insecureAuth:true,
         dateStrings : "date",
@@ -19,3 +41,9 @@ const mysql = require('mysql2/promise');
 });
 
 module.exports=pool;
+}).catch((err) => {
+  console.error('Error reading secret:', err);
+});
+
+
+
