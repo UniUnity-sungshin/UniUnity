122a98e43dfe9362e29290a7dada0cca5ab0cc31
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-12-14 14:29:04 +0900

    Update db.js

--- app/config/db.js
+++ app/config/db.js
@@ -1,68 +1,45 @@
-// // dbConnector.js
-// const k8s = require('@kubernetes/client-node');
+// dbConnector.js
+const k8s = require('@kubernetes/client-node');
 
-// async function getDatabasePool() {
-//   const kc = new k8s.KubeConfig();
-//   kc.loadFromDefault();
-//   const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+async function getDatabasePool() {
+  const kc = new k8s.KubeConfig();
+  kc.loadFromDefault();
+  const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
-//   const secretName = 'db-secret';
-//   const namespace = 'default';  // 네임스페이스에 주의
+  const secretName = 'db-secret';
+  const namespace = 'default';  // 네임스페이스에 주의
 
-//   try {
-//     // 시크릿 가져오기
-//     const response = await k8sApi.readNamespacedSecret(secretName, namespace);
+  try {
+    // 시크릿 가져오기
+    const response = await k8sApi.readNamespacedSecret(secretName, namespace);
 
-//     // 환경 변수 설정
-//     const dbHost = Buffer.from(response.body.data.DB_HOST, 'base64').toString('utf-8');
-//     const dbPort = Buffer.from(response.body.data.DB_PORT, 'base64').toString('utf-8');
-//     const dbUser = Buffer.from(response.body.data.DB_USER, 'base64').toString('utf-8');
-//     const dbPassword = Buffer.from(response.body.data.DB_PW, 'base64').toString('utf-8');
-//     const dbName = Buffer.from(response.body.data.DB_NAME, 'base64').toString('utf-8');
-
-//     // MySQL 연결 풀 생성
-//     const mysql = require('mysql2/promise');
-
-//     const pool = mysql.createPool({
-//       host: dbHost,
-//       user: dbUser,
-//       port: dbPort,
-//       database: dbName,
-//       password: dbPassword,
-//       waitForConnections: true,
-//       insecureAuth: true,
-//       dateStrings: "date",
-//       connectionLimit: 1000,
-//       waitForConnections: false
-//     });
-
-//     console.log(dbHost, dbName, dbUser, dbPassword, dbPort);  // 이제는 변수를 직접 사용
-//     console.log('db.js파일 Pool:', pool);
-//     return pool;
-//   } catch (err) {
-//     console.error('Error reading secret:', err);
-//     throw err; // 호출자에게 에러 전파
-//   }
-// }
-// const pool= getDatabasePool()
-
-// console.log('db.js파일: getDatabasePool:', pool);
-
-// module.exports = pool;
-
+    // 환경 변수 설정
+    const dbHost = Buffer.from(response.body.data.DB_HOST, 'base64').toString('utf-8');
+    const dbPort = Buffer.from(response.body.data.DB_PORT, 'base64').toString('utf-8');
+    const dbUser = Buffer.from(response.body.data.DB_USER, 'base64').toString('utf-8');
+    const dbPw = Buffer.from(response.body.data.DB_PW, 'base64').toString('utf-8');
+    const dbName = Buffer.from(response.body.data.DB_NAME, 'base64').toString('utf-8');
+    
+    return { dbHost:dbHost,dbPort:dbPort,dbUser:dbUser,dbPw:dbPw,dbName:dbName }
+  } catch (err) {
+    console.error('Error reading secret:', err);
+    throw err; // 호출자에게 에러 전파
+  }
+  
+}
 //db.js
 // require('dotenv').config();
-
+const dbInfo = getDatabasePool()
+console.log(`dbInfo:${dbInfo} `)
 const mysql = require('mysql2/promise');
 
 // MySQL 연결 풀 생성
-
-  const pool = mysql.createPool({
-    host:"34.64.187.145",
-        user:"monami",
-        port:3306,
-        database:"uniunityDB",
-        password:"mypass",
+const pool = mysql.createPool({
+    host:dbInfo.dbHost,
+        user:dbInfo.user,
+        port:dbInfo.port,
+        database:dbInfo.database,
+        password:dbInfo.password,
         waitForConnections:true,
         insecureAuth:true,
         dateStrings : "date",
